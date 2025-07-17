4ebc5f739962c6a061a01b1b99ca40583ceb5543
he0603 <junlong.he@elephantrobotics.com>
2025-07-17 13:42:43 +0800

    update

--- testcases/mycobot_320_123/test_39_pause.py
+++ testcases/mycobot_320_123/test_39_pause.py
@@ -11,7 +11,6 @@ from settings import Mycobot320Base
 cases = get_test_data_from_excel(Mycobot320Base.TEST_DATA_FILE, "pause")
 
 normal_cases = [case for case in cases if case.get("test_type") == "normal"]
-logic_cases = [case for case in cases if case.get("test_type") == "logic"]
 
 @pytest.fixture(scope="module")
 def device():
@@ -29,15 +28,17 @@ def device():
 @allure.story("正常用例")
 @pytest.mark.parametrize("case", normal_cases, ids=[case["title"] for case in normal_cases])
 def test_pause1(device, case):
+    ID = case["ID"]
     title = case["title"]
     expected = case["expect_data"]
 
     logger.info(f'》》》》》用例【{title}】开始测试《《《《《')
     logger.debug(f'test_api:{case["api"]}')
 
+    with allure.step('控制机械臂运动'):
+
 
     with allure.step(f"调用 pause 接口"):
-        device.m.send_angles(device.zero_angles, device.speed)
         set_res = device.m.pause()
         get_res = device.m.is_paused()
         logger.debug(f"set_res返回:{set_res},get_res返回:{get_res}")
@@ -45,15 +46,15 @@ def test_pause1(device, case):
     with allure.step("断言返回值类型为 int"):
         assert isinstance(set_res, int), f"返回类型错误,应为{type(expected)},实际为 {type(set_res)}"
 
-    with allure.step("断言设置返回值"):
+    with allure.step("断言pause返回值"):
         allure.attach(str(expected), name="set接口期望值", attachment_type=allure.attachment_type.TEXT)
         allure.attach(str(set_res), name="set接口实际值", attachment_type=allure.attachment_type.TEXT)
         assert set_res == expected, f"用例【{title}】断言失败，期望 {expected}，实际 {set_res}"
 
-    with allure.step("断言获取返回值"):
-        allure.attach(str(coords), name="get接口期望值", attachment_type=allure.attachment_type.TEXT)
+    with allure.step("断言is_paused返回值"):
+        allure.attach(str(expected), name="get接口期望值", attachment_type=allure.attachment_type.TEXT)
         allure.attach(str(get_res), name="get接口实际值", attachment_type=allure.attachment_type.TEXT)
-        assert get_res == coords, f"用例【{title}】断言失败，期望 {coords}，实际 {get_res}"
+        assert get_res == expected, f"用例【{title}】断言失败，期望 {expected}，实际 {get_res}"
 
     logger.info(f'✅ 用例【{title}】测试通过')
     logger.info(f'》》》》》用例【{case["title"]}】测试完成《《《《《')
