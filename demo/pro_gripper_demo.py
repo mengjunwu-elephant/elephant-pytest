af2f6d65036080f74896ee2f095b0f687ae8e464
mengjunwu-elephant <mengjun.wu@elephantrobotics.com>
2025-07-22 13:37:29 +0800

    fix bug

--- demo/pro_gripper_demo.py
+++ demo/pro_gripper_demo.py
@@ -1,11 +1,20 @@
 from elegripper.elegripper import Gripper
 
 
-m = Gripper("com3",id=10)
+# m = Gripper("com3",id=10)
 # m.set_gripper_Id(-1)
 # m.set_gripper_Id(10)
-m.set_gripper_Id(14)
+# m.set_gripper_Id(14)
 # m.set_gripper_value(101)
 # print(m.set_gripper_mini_pressure(-1))
 # print(m.get_gripper_Id())
 # m.set_gripper_baud(0)
+
+def device():
+    for i in range(5):
+        yield i
+
+dev = device()
+
+print(next(dev))
+print(next(dev))
--- testcases/my_hand/test_get_firmware_version.py
+++ testcases/my_hand/test_get_firmware_version.py
@@ -31,7 +31,7 @@ def test_get_firmware_version(device, case):
         logger.debug(f"接口返回：{response}")
 
     with allure.step("断言返回值类型为 float"):
-        assert isinstance(response, float), f"返回类型错误，应为 int，实际为 {type(response)}"
+        assert isinstance(response, float), f"返回类型错误，应为 float，实际为 {type(response)}"
 
     with allure.step("断言接口返回结果"):
         allure.attach(str(expected), name="期望值", attachment_type=allure.attachment_type.TEXT)
