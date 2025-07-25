f425ca69c68fd51c09c7fe2c15e8b4bf6cb15535
chenmuyang <2474750066@qq.com>
2025-07-25 17:55:20 +0800

    fix bug

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ if __name__ == '__main__':
 
 
 
-    product_name = input("请输入数字选择需要测试的产品:\n"
-                         "1: mercury\n"
-                         "2: mercury_pro_gripper\n"
-                         "3: mercury_my_hand\n"
-                         "4: pro_gripper\n"
-                         "5: my_hand\n"
-                         "6: mycobot_280\n"
-                         "7: mycobot_320\n"
-                         )
-
-    # 获取对应用例路径
-    case_path = CASES_DIR.get(product_name)
-    if not case_path:
-        print("输入错误，请输入1-7之间的数字。")
-        exit(1)
+    # product_name = input("请输入数字选择需要测试的产品:\n"
+    #                      "1: mercury\n"
+    #                      "2: mercury_pro_gripper\n"
+    #                      "3: mercury_my_hand\n"
+    #                      "4: pro_gripper\n"
+    #                      "5: my_hand\n"
+    #                      "6: mycobot_280\n"
+    #                      "7: mycobot_320\n"
+    #                      )
+    #
+    # # 获取对应用例路径
+    # case_path = CASES_DIR.get(product_name)
+    # if not case_path:
+    #     print("输入错误，请输入1-7之间的数字。")
+    #     exit(1)
 
     # # 生成报告文件名（带时间戳）
     # now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
@@ -44,7 +44,7 @@ if __name__ == '__main__':
 
 # 执行 pytest 并生成 allure 原始结果
     pytest.main(["-s",
-        case_path,
+       "testcases/mycobot_280",
         f"--alluredir={REPORT_DIR}"
     ])
     pytest.main()
