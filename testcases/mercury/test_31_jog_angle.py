d68c7186e945a618972e8a85bed7647745040039
mengjunwu-elephant <mengjun.wu@elephantrobotics.com>
2026-01-06 10:10:01 +0800

    adapt firmware_v2.0.3

--- testcases/mercury/test_31_jog_angle.py
+++ testcases/mercury/test_31_jog_angle.py
@@ -3,7 +3,7 @@ import allure
 from time import sleep
 
 from pymycobot.error import MercuryDataException
-from common1 import logger
+from common1 import logger, assert_almost_equal
 from common1.test_data_handler import get_test_data_from_excel
 from settings import MercuryBase
 
@@ -43,19 +43,21 @@ def test_jog_angle_left(device, case):
 
     with allure.step("发送 jog_angle 指令（左臂）"):
         response = device.ml.jog_angle(joint, param, speed)
-        sleep(2)
+        device.wait()
 
     with allure.step("判断是否到达软件限位（左臂）"):
         current_angle = device.ml.get_angle(joint)
-        target = device.min_limit[joint - 1] if param == 0 else device.max_limit[joint - 1]
-        reached = device.is_in_position(target, current_angle)
-        assert reached == 1, f"左臂 {joint} 号关节未到达限位, 当前角度: {current_angle}"
+        target = device.angles_min[joint - 1] if param == 0 else device.angles_max[joint - 1]
+        allure.attach(str(target), name="左臂期望", attachment_type=allure.attachment_type.TEXT)
+        allure.attach(str(current_angle), name="左臂实际", attachment_type=allure.attachment_type.TEXT)
+        assert_almost_equal(target,current_angle,tol=1,name='左臂jog角度运动'),f"左臂响应不一致，期望: {target}，实际: {current_angle}"
 
     with allure.step("断言响应结果正确（左臂）"):
         assert isinstance(response, int), f"返回值类型错误: {type(response)}"
         assert response == case["l_expect_data"], f"期望值: {case['l_expect_data']}, 实际值: {response}"
 
-    logger.info(f"✅ 用例【{title}】测试成功")
+    logger.info(f"✅ 用例【{case['title']}】测试通过")
+    logger.info(f"》》》用例【{case['title']}】测试完成《《《")
 
 
 @allure.feature("jog_angle 接口测试")
@@ -72,19 +74,21 @@ def test_jog_angle_right(device, case):
 
     with allure.step("发送 jog_angle 指令（右臂）"):
         response = device.mr.jog_angle(joint, param, speed)
-        sleep(2)
+        device.wait()
 
     with allure.step("判断是否到达软件限位（右臂）"):
         current_angle = device.mr.get_angle(joint)
-        target = device.min_limit[joint - 1] if param == 0 else device.max_limit[joint - 1]
-        reached = device.is_in_position(target, current_angle)
-        assert reached == 1, f"右臂 {joint} 号关节未到达限位, 当前角度: {current_angle}"
+        target = device.angles_min[joint - 1] if param == 0 else device.angles_max[joint - 1]
+        allure.attach(str(target), name="左臂期望", attachment_type=allure.attachment_type.TEXT)
+        allure.attach(str(current_angle), name="左臂实际", attachment_type=allure.attachment_type.TEXT)
+        assert_almost_equal(target, current_angle, tol=1,name='右臂jog角度运动'), f"左臂响应不一致，期望: {target}，实际: {current_angle}"
 
     with allure.step("断言响应结果正确（右臂）"):
         assert isinstance(response, int), f"返回值类型错误: {type(response)}"
         assert response == case["r_expect_data"], f"期望值: {case['r_expect_data']}, 实际值: {response}"
 
-    logger.info(f"✅ 用例【{title}】测试成功")
+    logger.info(f"✅ 用例【{case['title']}】测试通过")
+    logger.info(f"》》》用例【{case['title']}】测试完成《《《")
 
 
 @allure.feature("jog_angle 接口测试")
