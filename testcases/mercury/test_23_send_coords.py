94f2b4e14c4226be9eac325d0b2a7ce05f4ece18
mengjunwu-elephant <mengjun.wu@elephantrobotics.com>
2026-01-30 17:37:36 +0800

    fix bug

--- testcases/mercury/test_23_send_coords.py
+++ testcases/mercury/test_23_send_coords.py
@@ -81,8 +81,8 @@ def test_send_coords_out_limit_left(device, case):
     speed = case["speed"]
 
     logger.info(f"》》》》》用例【{case['title']}】开始测试（左臂）《《《《《")
-    with allure.step("左臂发送非法坐标，断言抛出 MercuryDataException") as exc_info:
-        with pytest.raises(MercuryDataException):
+    with allure.step("左臂发送非法坐标，断言抛出 MercuryDataException"):
+        with pytest.raises(MercuryDataException) as exc_info:
             device.ml.send_coords(param, speed)
 
     logger.info(f"✅ 用例【{case['title']}】触发了预期异常: {exc_info.value}")
@@ -98,8 +98,8 @@ def test_send_coords_out_limit_right(device, case):
     speed = case["speed"]
 
     logger.info(f"》》》》》用例【{case['title']}】开始测试（右臂）《《《《《")
-    with allure.step("右臂发送非法坐标，断言抛出 MercuryDataException") as exc_info:
-        with pytest.raises(MercuryDataException):
+    with allure.step("右臂发送非法坐标，断言抛出 MercuryDataException"):
+        with pytest.raises(MercuryDataException) as exc_info:
             device.mr.send_coords(param, speed)
 
     logger.info(f"✅ 用例【{case['title']}】触发了预期异常: {exc_info.value}")
